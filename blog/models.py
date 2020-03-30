1afa4bbafe458a89781dbe631f47995eb3ab2dad
mayank <mayank@gmail.com>
2020-03-31 01:04:46 +0530

    blog and jobs done

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+	title=models.CharField(max_length=100)
+	image=models.ImageField(upload_to='images/')
+	pub_date=models.DateField(auto_now=True)
+	body=models.TextField()
+
