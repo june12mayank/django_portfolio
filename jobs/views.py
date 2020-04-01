2e0efc1eacd7663b3075d55ee8b9ad8e7bd22a7c
mayank <mayank@gmail.com>
2020-04-01 15:32:36 +0530

    job auto add

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import jobs
 
-# Create your views here.
+def home(request):
+	Jobs=jobs.objects
+	return render(request,'jobs/home.html',{'Jobs':Jobs})
